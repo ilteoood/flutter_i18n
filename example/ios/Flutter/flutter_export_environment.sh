f585d9bfdac3b7bb0f50ecbdc676f1d2528b3b18
Anton <anton.matrenin@introduct.tech>
2020-03-08 18:47:30 +0200

    Change environment file

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/anton/Downloads/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/anton/flutter/flutter_i18n_fork/example"
+export "FLUTTER_ROOT=/usr/local/Cellar/flutter/stable"
+export "FLUTTER_APPLICATION_PATH=/Users/a34p/Documents/git/flutter_i18n/example"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/anton/Downloads/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/usr/local/Cellar/flutter/stable/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
