0006d2408a2025e44c0f834e7f25139204aa82ad
Anton <anton.matrenin@introduct.tech>
2020-03-13 10:24:19 +0200

    Add loader tests

--- test/test_loader.dart
+++ test/test_loader.dart
@@ -15,11 +15,23 @@ class TestLoader extends FileTranslationLoader {
 
   @override
   Future<String> loadString(String fileName, String extension) async {
+    if (fileName == '_en') {
+      // Throw the error for all cases with this locale.
+      throw new Error();
+    }
+
+    if (fileName == 'uk' && extension == 'json') {
+      // Throw the error only for json extension with this locale.
+      throw new Error();
+    }
+
     return '''
       {
         "keySingle": "valueSingle",
         "keyPlural-1": "valuePlural-1",
-        "keyPlural-2": "valuePlural-2"
+        "keyPlural-2": "valuePlural-2",
+        "fileName": "$fileName",
+        "extension": "$extension"
       }
     ''';
   }
