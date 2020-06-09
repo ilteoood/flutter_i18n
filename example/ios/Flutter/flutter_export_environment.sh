e5a7b906686b7fec69903da4cd6b4a92535ba6b0
Anton <anton.matrenin@introduct.tech>
2020-06-09 11:21:42 +0300

    issues/107

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
-# This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/anton/Downloads/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/anton/flutter/flutter_i18n/example"
-export "FLUTTER_TARGET=lib/main.dart"
-export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=/Users/anton/Downloads/flutter/bin/cache/artifacts/engine/ios"
-export "FLUTTER_BUILD_NAME=1.0.0"
+#!/bin/sh	#!/bin/sh
+# This is a generated file; do not edit or check into version control.	# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/opt/hostedtoolcache/flutter/1.17.1-stable/x64"	export "FLUTTER_ROOT=/Users/anton/Downloads/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/runner/work/flutter_i18n/flutter_i18n/example"	export "FLUTTER_APPLICATION_PATH=/Users/anton/flutter/flutter_i18n/example"
+export "FLUTTER_TARGET=lib/main.dart"	export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"	export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"	export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"	export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/opt/hostedtoolcache/flutter/1.17.1-stable/x64/bin/cache/artifacts/engine/ios"	export "FLUTTER_FRAMEWORK_DIR=/Users/anton/Downloads/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"	export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
