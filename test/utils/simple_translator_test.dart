0a7bbc20d6717464973c1ee191c15ccaf56bec9f
tomk9 <tomaszsz95@gmail.com>
2022-05-24 13:22:01 +0200

    Added unit tests for SimpleTranslator

---
+++ test/utils/simple_translator_test.dart
@@ -0,0 +1,69 @@
+import 'package:flutter_i18n/utils/simple_translator.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('SimpleTranslator', () {
+    test('should have initial values', () {
+      final instance = SimpleTranslator(
+        null,
+        'object.key1',
+        '.',
+      );
+      expect(instance.decodedMap, null);
+      expect(instance.key, 'object.key1');
+      expect(instance.keySeparator, '.');
+    });
+
+    test('should translate from decoded map', () {
+      final instance = SimpleTranslator(
+        {
+          'object': {
+            'key1': 'value1',
+          },
+        },
+        'object.key1',
+        '.',
+      );
+      final translation = instance.translate();
+      expect(translation, 'value1');
+    });
+
+    test('should return key when value is missing in decoded map', () {
+      final instance = SimpleTranslator(
+        null,
+        'object.key1',
+        '.',
+        missingKeyTranslationHandler: (key) {},
+      );
+      final translation = instance.translate();
+      expect(translation, 'object.key1');
+    });
+
+    test('should calculate submap from key', () {
+      final instance = SimpleTranslator(
+        {
+          'object': {
+            'key1': 'value1',
+          },
+        },
+        'object.key1',
+        '.',
+      );
+      final submap = instance.calculateSubmap('object.key1');
+      expect(
+        submap,
+        {'key1': 'value1'},
+      );
+    });
+
+    test('should calculate empty submap from empty key', () {
+      final instance = SimpleTranslator(
+        null,
+        'object.key1',
+        '.',
+      );
+      final submap = instance.calculateSubmap('');
+      expect(submap, {});
+    });
+  });
+}
