78d16cca17a2d44fba1f9da3a7c05859e6e47c0e
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2025-10-28 21:50:30 +0100

    chore: removed html package

--- lib/flutter_i18n_web.dart
+++ lib/flutter_i18n_web.dart
@@ -1,12 +1,12 @@
 import 'dart:async';
+
 // In order to *not* need this ignore, consider extracting the "web" version
 // of your plugin as a separate package, instead of inlining it in the same
 // package as the core of your plugin.
 // ignore: avoid_web_libraries_in_flutter
-import 'dart:html' as html show window;
-
 import 'package:flutter/services.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:web/web.dart';
 
 /// A web implementation of the FlutterI18n plugin.
 class FlutterI18nWeb {
@@ -38,7 +38,7 @@ class FlutterI18nWeb {
 
   /// Returns a [String] containing the version of the platform.
   Future<String> getPlatformVersion() {
-    final version = html.window.navigator.userAgent;
+    final version = window.navigator.userAgent;
     return Future.value(version);
   }
 }
